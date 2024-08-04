2726307e04e2f64267f7ddcfca1ceccd30490a02
Erdem Sincer <Erdem Sincer@DESKTOP-OC5SD50>
2024-08-04 19:45:46 +0300

    Add project files.

---
+++ MyPortolio/DAL/Entities/Testimonial.cs
@@ -0,0 +1,11 @@
+﻿namespace MyPortolio.DAL.Entities
+{
+    public class Testimonial
+    {
+        public int TestimonialId { get; set; }
+        public int NameSurname { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int ImageUrl { get; set; }
+    }
+}
